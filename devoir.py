fd7ec39366dfa3b890a6fce483cd4865530a0e2d
Youko509 <saintcharleserico@gmail.com>
2023-02-10 13:46:22 -0500

    add text

--- devoir.py
+++ devoir.py
@@ -3,6 +3,6 @@ import re
 def convert_hashtags(text):
     return re.sub(r'#(\w+)', r'<a href="\1">#\1</a>', text)
 
-text = "Erico #tag"
+text = "fok nou  #tag on moun ki #sou"
 converted_text = convert_hashtags(text)
 print(converted_text)
